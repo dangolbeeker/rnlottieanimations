b6e5679ce2e800bbde8f8e2ab7c4a5b43ab7d3a6
dangolbeeker <scottb305@outlook.com>
2020-10-28 21:43:11 -0400

    Scalpel

---
+++ App.js
@@ -0,0 +1,26 @@
+import React from 'react';
+// import LottieView from 'lottie-react-native';
+import {useEffect, useState, useRef } from 'react';
+import {createStackNavigator}from '@react-navigation/stack'
+// import { useLinkProps } from '@react-navigation/native';
+
+createStackNavigator =useState[['']]
+
+const Appcontainer = createAppContainer(AppWithNavigationState)
+
+// let Navigator {
+//     (useLinkProps);
+// }
+
+const App = () => {
+    return (
+        <Appcontainer />
+    )
+
+}
+{/* <Appcontainer><Navigator></Navigator></Appcontainer> */}
+
+
+
+
+
